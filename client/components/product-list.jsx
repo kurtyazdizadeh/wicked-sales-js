d7d8bbe9dedd316780f3cac33a2721dafece735e
kurtyazdizadeh <kurtyazdizadeh@gmail.com>
2020-04-24 00:59:36 +0000

    created product-list component, defined getProducts method with GET request, need to build out render method

---
+++ client/components/product-list.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
+
+  getProducts() {
+    fetch('/api/products')
+      .then(res => {
+        return res.json();
+      })
+      .then(productArray => {
+        this.setState({ products: productArray });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
+  render() {
+    return (
+      <div>test</div>
+    );
+  }
+}
+
+export default ProductList;
