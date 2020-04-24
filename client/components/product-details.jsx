853b23699db8ea0bc362fde6e3ef84e2937d09f2
kurtyazdizadeh <kurtyazdizadeh@gmail.com>
2020-04-24 18:19:58 +0000

    made return for productDetails component

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -8,8 +8,39 @@ class ProductDetails extends React.Component {
     };
   }
 
+  componentDidMount() {
+    fetch('api/products/1')
+      .then(res => res.json())
+      .then(product => {
+        this.setState({ product: product });
+      })
+      .catch(err => console.error(err));
+  }
+
   render() {
-    return <div>Product Details go here</div>;
+    const { name, image, price, shortDescription, longDescription } = this.state;
+    return (
+      this.state.product
+        ? (
+          <div className="col d-flex">
+            <div className="row">
+              <h4>&gt; Back to Catalog</h4>
+            </div>
+            <div className="row">
+              <img src={image} alt={name} className="col-6"/>
+              <div className="product-info d-flex flex-column col-6">
+                <h2 className="font-weight-bold">{name}</h2>
+                <h3 className="text-secondary">${(price / 100).toFixed(2)}</h3>
+                <p>{shortDescription}</p>
+              </div>
+              <div className="row">
+                {longDescription}
+              </div>
+            </div>
+          </div>
+        )
+        : <div>Loading...</div>
+    );
   }
 }
 
