c9b78c56d87d6288277a0e667482497149fd233b
kurtyazdizadeh <kurtyazdizadeh@gmail.com>
2020-04-24 01:55:39 +0000

    View Products for Sale | Front End

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -5,7 +5,7 @@ class ProductListItem extends React.Component {
     const { name, image, price, desc } = this.props;
     return (
       <div className="card m-2 col-3">
-        <img src={image} className="card-img-top h-50 mt-3 scale" alt={name} />
+        <img src={image} className="card-img-top mt-3 scale img-fluid" alt={name} />
         <div className="card-body">
           <h5 className="card-title font-weight-bold">{name}</h5>
           <h6 className="text-secondary">${price}</h6>
