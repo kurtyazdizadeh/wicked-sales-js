055f81caca06b6c73a36319cc0b3008e70b237ef
kurtyazdizadeh <kurtyazdizadeh@gmail.com>
2020-04-24 17:55:40 +0000

    added ProductDetails component with initial state and dummy render method

---
+++ client/components/product-details.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class ProductDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: null
+    };
+  }
+
+  render() {
+    return <div>Product Details go here</div>;
+  }
+}
+
+export default ProductDetails;
